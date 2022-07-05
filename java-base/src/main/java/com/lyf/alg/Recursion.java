a22c76eeb7255167bd76ca0309c07cb0dad74d58
lyf <674619459@qq.com>
2022-07-05 15:31:20 +0800

    [test git] add alg

--- java-base/src/main/java/com/lyf/alg/Recursion.java
+++ java-base/src/main/java/com/lyf/alg/Recursion.java
@@ -1,8 +1,21 @@
 package com.lyf.alg;
 
+import jdk.jfr.StackTrace;
+import org.junit.Test;
+
 /**
  * @author LYF
  * @date 2022/7/5
  */
 public class Recursion {
+    @Test
+    public void test1(){
+
+    }
+    /*
+    Fib
+     */
+    void recursion(int n){
+
+    }
 }
